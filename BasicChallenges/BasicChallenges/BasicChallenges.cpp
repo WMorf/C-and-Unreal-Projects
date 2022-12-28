07d5bf4b18b642299611440597aadf3d7527cd62
Wesley Morford <wesmorford@outlook.com>
2022-12-27 16:14:25 -0800

    Push

---
+++ BasicChallenges/BasicChallenges/BasicChallenges.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //HELLO WORLD CHALLENGE
+    cout << "Hello World!\n";
+    cout << "\n";
+
+    //C++ OUTPUT CHALLENGE
+    cout << "Output Challenge\n";
+    cout << "\n";
+
+    //C++ NEW LINES CHALLENGE
+    cout << "Make a new line by typing \\n like \nthis\n";
+    cout << "\n";
+
+
+    //VARIABLES CHALLENGE
+    char key = 'b'; //Single quotes needed
+    int num1 = 5;
+    int num2 = 4;
+    double dub1 = 5.4;
+    bool smart = true;
+    string name = "Bob";
+    const int num3 = 10;
+
+    cout << key;
+    cout << "\n";
+    cout << num1;
+    cout << "\n";
+    cout << num2;
+    cout << "\n";
+    cout << dub1;
+    cout << "\n";
+    cout << smart;
+    cout << "\n";
+    cout << name;
+    cout << "\n";
+    cout << num3;
+    cout << "\n";
+
+    cout << "\n";
+
+    //USER INPUT CHALLENGE
+    string input1 = "";
+    cout << "Input Text Here \n";
+    cin >> input1; // >> for input
+    cout << input1;
+    cout << "\n";
+
+    //NUMBERS CHALLENGE
+    float num4 = 7;
+    cout << num4;
+    cout << "\n";
+
+    //STRING CHALLENGE
+    string sent1 = "This is the start.";
+    string sent2 = "This is the end.";
+    cout << sent1 + " " + sent2;
+    cout << "\n";
+
+    //STRING FUNCTIONS CHALLENGE:
+
+
+}
+
+
