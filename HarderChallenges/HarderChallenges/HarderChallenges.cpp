56ead86d7a9801f9220e28c57a9797d269615ada
Wesley Morford <wesmorford@outlook.com>
2022-12-27 20:50:24 -0800

    Push

---
+++ HarderChallenges/HarderChallenges/HarderChallenges.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //MULTI LINE CODE COMMENTS CHALLENGE
+    cout << "//MULTI LINE CODE COMMENTS CHALLENGE\n";
+    
+    /*This is commented
+    over multiple lines*/
+    cout << "Nothing to see here\n";
+
+    cout << "\n";
+
+    //GETLINE() FUNCTION CHALLENGE
+    cout << "//GETLINE() FUNCTION CHALLENGE\n";
+
+    string fullName;
+    cout << "Type your full name\n";
+    getline(cin, fullName);
+    cout << fullName;
+    cout << "\n";
+
+    cout << "\n";
+
+    //MATH FUNCTIONS CHALLENGE
+    cout << "//MATH FUNCTIONS CHALLENGE\n";
+
+
+
+    cout << "\n";
+
+    //CONDITIONAL STATEMENTS CHALLENGE
+    cout << "//CONDITIONAL STATEMENTS CHALLENGE\n";
+
+
+
+    cout << "\n";
+
+    //WHILE LOOP CHALLENGE
+    cout << "//WHILE LOOP CHALLENGE\n";
+
+
+
+    cout << "\n";
+
+    //DO/WHILE LOOP CHALLENGE
+    cout << "//DO/WHILE LOOP CHALLENGE\n";
+
+
+
+    cout << "\n";
+
+    //FOR LOOP CHALLENGE
+    cout << "//FOR LOOP CHALLENGE\n";
+
+
+
+    cout << "\n";
+
+    //BREAK AND CONTINUE CHALLENGE
+    cout << "//BREAK AND CONTINUE CHALLENGE\n";
+
+
+
+    cout << "\n";
+
+    //ARRAY CHALLENGE
+    cout << "//ARRAY CHALLENGE\n";
+
+
+
+    cout << "\n";
+
+    //ARRAY LOOP CHALLENGE
+    cout << "//ARRAY LOOP CHALLENGE\n";
+
+
+
+    cout << "\n";
+
+    //REFERENCE VARIABLE CHALLENGE
+    cout << "//REFERENCE VARIABLE CHALLENGE\n";
+
+
+
+    cout << "\n";
+
+}
